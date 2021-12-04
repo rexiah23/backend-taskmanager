8acaf941df7431a4a3dd42f4087dd2218830f595
Lewis Lee <lewisforbusiness@gmail.com>
2021-12-04 12:52:15 -0500

    fix|fix issue where user is inable to add new task or list when there are no tasks or lists in database

--- routes/list.js
+++ routes/list.js
@@ -44,9 +44,19 @@ module.exports = (db) => {
     const firstQuery = `SELECT * FROM list ORDER BY id DESC LIMIT 1;`;
     db.query(firstQuery)
     .then((result) => {
-      const latestListId = result.rows[0].id;
-      const newListId = `list-${parseInt(latestListId[latestListId.length-1]) + 1}`;
+      let newListId = '';
+      console.log("Latest list id is:", result.rows);
+      //If there are no lists saved in database, set newListId as 'list-1'
+      if (!result.rows[0]) {
+        console.log('entered first if')
+        newListId = 'list-1';
+      } else {
+        console.log('entered second if')
+        const latestListId = result.rows[0].id;
+        newListId = `list-${parseInt(latestListId[latestListId.length-1]) + 1}`;
+      }
       const secondQuery = `INSERT INTO list(id, title) VALUES ($1, $2)`;
+      console.log('List id: ', newListId);
       const args = [newListId, title];
       db.query(secondQuery, args)
       .then(() => {
--- routes/task.js
+++ routes/task.js
@@ -1,10 +1,11 @@
+const e = require('express');
 const express = require('express');
 const res = require('express/lib/response');
 const router  = express.Router();
 
 module.exports = (db) => {
   //get all tasks and lists
-  router.get("/all", (req, res) => {
+  router.get("/all", (request, response) => {
     let query = `SELECT list.id as list_id, list.title as list_title, task.id as task_id, task.content as task_content FROM list LEFT JOIN task ON (task.list_id = list.id)`;
     db.query(query)
       .then(data => {
@@ -34,11 +35,51 @@ module.exports = (db) => {
           };
           lists[el.list_id].tasks.push(task);
         });
-        const response = {lists, listIds}
-        res.json({response});
+        const refactoredData = {lists, listIds}
+        response.json({refactoredData});
       })
   });
 
+
+
+  // let newListId = '';
+  // //If there are no lists saved in database, set newListId as 'list-1'
+  // if (!result.rows[0]) {
+  //   newListId = 'list-1';
+  // } else {
+  //   const latestListId = result.rows[0].id;
+  //   newListId = `list-${parseInt(latestListId[latestListId.length-1]) + 1}`;
+  // }
+  // const secondQuery = `INSERT INTO list(id, title) VALUES ($1, $2)`;
+  // const args = [newListId, title];
+  router.post("/add", (request, response) => {
+    const { title:content, listId } = request.body;
+    const firstQuery = `SELECT * FROM task ORDER BY id DESC LIMIT 1;`;
+    db.query(firstQuery)
+    .then((result) => {
+      let newTaskId = '';
+      //If there are no tasks saved in database, set newTasId as 'task-1'
+      if (!result.rows[0]) {
+        newTaskId = 'task-1';
+      } else {
+        const latestTaskId = result.rows[0].id;
+        newTaskId = `task-${parseInt(latestTaskId[latestTaskId.length-1]) + 1}`;
+      }
+      const secondQuery = `INSERT INTO task(id, content, list_id) VALUES ($1, $2, $3)`;
+      const args = [newTaskId, content, listId];
+      db.query(secondQuery, args)
+      .then(() => {
+        db.query(firstQuery)
+        .then(result => {
+          const insertedTaskValue = result.rows[0];
+          response.json({insertedTaskValue})
+        })
+        .catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
+  });
 
 
   router.delete("/delete/:id", (request, response) => {
